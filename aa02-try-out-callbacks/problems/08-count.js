4bc1900785f381efae6936044ea32e28fee16cfe
Erika <brandonerika96@gmail.com>
2024-04-02 11:51:03 -0400

    problem 8 done

--- aa02-try-out-callbacks/problems/08-count.js
+++ aa02-try-out-callbacks/problems/08-count.js
@@ -27,8 +27,37 @@ console.log(result4); // 0
 *******************************************************************************/
 
 function count(array, cb) {
-  // Your code here 
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (cb(array[i], i, array) === true) {
+      count++;
+    }
+  }
+
+  return count;
 }
+
+
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // 3
+
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('x');
+});
+console.log(result4); // 0
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
