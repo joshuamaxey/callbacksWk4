b1ec50c86174579fc248d929308c5eff683740ec
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-02 20:01:02 -0500

    14 done

--- aa02-try-out-callbacks/problems/14-map-mutator.js
+++ aa02-try-out-callbacks/problems/14-map-mutator.js
@@ -18,9 +18,28 @@ mapMutator(arr2, function (el, i) {
 console.log(arr2); // [ 0, 9, 20 ]
 *******************************************************************************/
 
+//accepts an array and a callback function
+//pass each element AND each index into the callback function.
+//use splice (??) to remove the element at the current index of the array and replce it with the modified element
+//return the mutated array
+
 function mapMutator(array, cb) {
-  // Your code here 
+  for (i = 0; i < array.length; i++) {
+    array[i] = cb(array[i], i);
+  }
 }
+
+let arr1 = [4, 2, 6, 5];
+mapMutator(arr1, function (el) {
+    return el * 2;
+});
+console.log(arr1);  // [ 8, 4, 12, 10 ]
+
+let arr2 = [8, 9, 10];
+mapMutator(arr2, function (el, i) {
+    return el * i;
+});
+console.log(arr2); // [ 0, 9, 20 ]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
